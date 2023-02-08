e2fb006be4bfd9361ef8075c5fa55c52b99c942d
unknown <rishilpatel157@gmail.com>
2023-02-08 20:56:55 +0530

    prime.js is rectified

--- prime.js
+++ prime.js
@@ -1,17 +1,19 @@
 //to find if the number is prime or not.
 
-
 let num = 4;
 let i = 1;
 let count = 0;
-while (i <= 13) {
+while (i <= num) {
 
-
-  
-
+  if(num%i==0){
+    count++
+    
+  }  
+i++
+   }
 if (count == 2) {
   console.log("Yes");
 }
 else {
   console.log("No");
-}   
+}
